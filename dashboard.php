f098f41cb8465308416fe846b1ba947fe72c8716
Suhail Ahmad <113935500+suhail22a@users.noreply.github.com>
2024-10-18 18:54:55 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -2,11 +2,9 @@
 session_start();
 error_reporting(0);
 include('includes/config.php');
-if(strlen($_SESSION['login'])==0)
-  { 
-header('location:index.php');
-}
-else{?>
+if(strlen($_SESSION['login'])==0) { 
+    header('location:index.php');
+} else { ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -21,78 +19,125 @@ else{?>
     <link href="assets/css/font-awesome.css" rel="stylesheet" />
     <!-- CUSTOM STYLE  -->
     <link href="assets/css/style.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <!-- GOOGLE FONT -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
-
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+    <style>
+        body {
+            background: linear-gradient(to right, #6a11cb, #2575fc);
+            color: #fff;
+            font-family: 'Open Sans', sans-serif;
+            padding-top: 50px;
+        }
+        .header-line {
+            font-size: 30px;
+            text-align: center;
+            margin-bottom: 40px;
+            color: #ffcc00;
+            animation: fadeInDown 1s ease-in-out;
+        }
+        .back-widget-set {
+            background-color: #ffffff;
+            color: #333;
+            transition: transform 0.4s, box-shadow 0.4s;
+            border-radius: 10px;
+            box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
+            padding: 20px;
+            text-align: center;
+            background: linear-gradient(145deg, #f7f7f7, #e6e6e6);
+        }
+        .back-widget-set:hover {
+            transform: translateY(-10px);
+            box-shadow: 0 10px 30px rgba(0, 0, 0, 0.3);
+        }
+        .back-widget-set i {
+            animation: bounce 2s infinite;
+            color: #333;
+        }
+        .alert h3 {
+            font-size: 24px;
+            font-weight: bold;
+        }
+        @keyframes bounce {
+            0%, 100% {
+                transform: translateY(0);
+            }
+            50% {
+                transform: translateY(-10px);
+            }
+        }
+        @keyframes fadeInDown {
+            from {
+                opacity: 0;
+                transform: translateY(-20px);
+            }
+            to {
+                opacity: 1;
+                transform: translateY(0);
+            }
+        }
+        @media (max-width: 768px) {
+            .header-line {
+                font-size: 24px;
+            }
+        }
+    </style>
 </head>
 <body>
-      <!------MENU SECTION START-->
-<?php include('includes/header.php');?>
-<!-- MENU SECTION END-->
+    <!------MENU SECTION START-->
+    <?php include('includes/header.php'); ?>
+    <!-- MENU SECTION END-->
+
     <div class="content-wrapper">
-         <div class="container">
-        <div class="row pad-botm">
-            <div class="col-md-12">
-                <h4 class="header-line">User DASHBOARD</h4>
-                
-                            </div>
+        <div class="container">
+            <div class="row pad-botm">
+                <div class="col-md-12">
+                    <h4 class="header-line animate__animated animate__fadeInDown">User DASHBOARD</h4>
+                </div>
+            </div>
 
-        </div>
-             
-             <div class="row">
-
-
-
-            
-                 <div class="col-md-3 col-sm-3 col-xs-6">
-                      <div class="alert alert-info back-widget-set text-center">
-                            <i class="fa fa-bars fa-5x"></i>
-<?php 
-$sid=$_SESSION['stdid'];
-$sql1 ="SELECT id from tblissuedbookdetails where StudentID=:sid";
-$query1 = $dbh -> prepare($sql1);
-$query1->bindParam(':sid',$sid,PDO::PARAM_STR);
-$query1->execute();
-$results1=$query1->fetchAll(PDO::FETCH_OBJ);
-$issuedbooks=$query1->rowCount();
-?>
-
-                            <h3><?php echo htmlentities($issuedbooks);?> </h3>
-                            Book Issued
-                        </div>
+            <div class="row">
+                <div class="col-md-3 col-sm-3 col-xs-6">
+                    <div class="alert alert-info back-widget-set text-center">
+                        <i class="fa fa-bars fa-5x"></i>
+                        <?php 
+                        $sid=$_SESSION['stdid'];
+                        $sql1 ="SELECT id from tblissuedbookdetails where StudentID=:sid";
+                        $query1 = $dbh -> prepare($sql1);
+                        $query1->bindParam(':sid',$sid,PDO::PARAM_STR);
+                        $query1->execute();
+                        $results1=$query1->fetchAll(PDO::FETCH_OBJ);
+                        $issuedbooks=$query1->rowCount();
+                        ?>
+                        <h3><?php echo htmlentities($issuedbooks); ?> </h3>
+                        Book Issued
                     </div>
-             
-               <div class="col-md-3 col-sm-3 col-xs-6">
-                      <div class="alert alert-warning back-widget-set text-center">
-                            <i class="fa fa-recycle fa-5x"></i>
-<?php 
-$rsts=0;
-$sql2 ="SELECT id from tblissuedbookdetails where StudentID=:sid and ReturnStatus=:rsts";
-$query2 = $dbh -> prepare($sql2);
-$query2->bindParam(':sid',$sid,PDO::PARAM_STR);
-$query2->bindParam(':rsts',$rsts,PDO::PARAM_STR);
-$query2->execute();
-$results2=$query2->fetchAll(PDO::FETCH_OBJ);
-$returnedbooks=$query2->rowCount();
-?>
+                </div>
 
-                            <h3><?php echo htmlentities($returnedbooks);?></h3>
-                          Books Not Returned Yet
-                        </div>
+                <div class="col-md-3 col-sm-3 col-xs-6">
+                    <div class="alert alert-warning back-widget-set text-center">
+                        <i class="fa fa-recycle fa-5x"></i>
+                        <?php 
+                        $rsts=0;
+                        $sql2 ="SELECT id from tblissuedbookdetails where StudentID=:sid and ReturnStatus=:rsts";
+                        $query2 = $dbh -> prepare($sql2);
+                        $query2->bindParam(':sid',$sid,PDO::PARAM_STR);
+                        $query2->bindParam(':rsts',$rsts,PDO::PARAM_STR);
+                        $query2->execute();
+                        $results2=$query2->fetchAll(PDO::FETCH_OBJ);
+                        $returnedbooks=$query2->rowCount();
+                        ?>
+                        <h3><?php echo htmlentities($returnedbooks); ?></h3>
+                        Books Not Returned Yet
                     </div>
+                </div>
+            </div>
         </div>
-
-
-            
     </div>
-    </div>
-     <!-- CONTENT-WRAPPER SECTION END-->
-    <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
-    <!-- CORE JQUERY  -->
+
+    <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE LOADING TIME -->
     <script src="assets/js/jquery-1.10.2.js"></script>
-    <!-- BOOTSTRAP SCRIPTS  -->
     <script src="assets/js/bootstrap.js"></script>
-      <!-- CUSTOM SCRIPTS  -->
     <script src="assets/js/custom.js"></script>
 </body>
 </html>
